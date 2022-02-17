76a10477b725f1efe0f72f5876aaf3ab32074e78
gilbertchristian <gilbert.spenzha23@gmail.com>
2022-02-17 20:19:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def swapPositions(list, pos1, pos2):
+def swap(list, pos1, pos2):
     list[pos1], list[pos2] = list[pos2], list[pos1]
     return list
+
+
+def convert_ascii(text):
+    result = []
+    for letter in text:
+        result.append(ord(letter))
+
+    return result
+
+
+def repeat(text):
+    length = len(text)
+    if len(text) == 256:
+        return text
+    else:
+        for i in range(256):
+            text.append(text[i % length])
+    return(text)
 
 
 def KSA(key):
@@ -10,9 +28,8 @@ def KSA(key):
 
     j = 0
     for i in range(256):
-        k = int(key[i % len(key_bit)])
-        j = (j + S[i] + k) % 256
-        swapPositions(S, i, j)
+        j = (j + S[i] + key[i]) % 256
+        swap(S, i, j)
 
     return S
 
@@ -20,28 +37,63 @@ def KSA(key):
 def PRGA(plaintext, S):
     i = 0
     j = 0
-    ciphertext = []
+    keystream = []
 
     for idx in range(len(plaintext)):
         i = (i+1) % 256
         j = (j + S[i]) % 256
-        swapPositions(S, i, j)
+        swap(S, i, j)
         t = (S[i] + S[j]) % 256
         u = S[t]
-        c = u ^ int(plaintext[idx])
-        ciphertext.append(c)
+        # c = u ^ int(plaintext[idx])
+        keystream.append(u)
 
     # ciphertext = ' '.join(c)
 
-    return ciphertext
+    return keystream
+
+
+# def convert_binary(text):
+#     result = []
+
+#     for idx in text:
+#         result.append(format(ord(idx), 'b'))
+
+#     return result
+
+def convert_binary(n):
+    return int(bin(n).replace("0b", ""))
+
+
+def encrypt(plaintext, keystream):
+    result = []
+    for i in range(len(plaintext)):
+        pt = convert_binary(plaintext[i])
+        ks = convert_binary(keystream[i])
+        print(pt, ks)
+        ct = pt ^ ks
+        result.append(ct)
+    return result
+    # print(convert_binary(plaintext))
+    # print(convert_binary(keystream))
 
 
 plaintext = list(input("Enter your plaintext: "))
-plaintext_bit = list(''.join(format(ord(i), '08b') for i in plaintext))
+plaintext_ascii = list(convert_ascii(plaintext))
 key = input("Enter your key: ")
-key_bit = list(''.join(format(ord(i), '08b') for i in key))
+key_ascii = list(convert_ascii(key))
 
-print("key", key_bit)
-ksa_S = KSA(key_bit)
-prga = PRGA(plaintext_bit, ksa_S)
-print(prga)
+print("key", key_ascii, "\n")
+
+repeated_key = repeat(key_ascii)
+
+# print("repeat", repeated_key, "\n")
+# print(repeat(key_ascii))
+
+ksa = KSA(repeated_key)
+# print(ksa, "\n")
+prga = PRGA(plaintext, ksa)
+# print(prga)
+
+encrypted_text = encrypt(plaintext_ascii, prga)
+print(encrypted_text)
